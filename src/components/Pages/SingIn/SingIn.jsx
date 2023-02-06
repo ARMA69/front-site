86e2133d7a7894d735255bd72474301edece5087
ARMA69 <OleksandrRizun@gmail.com>
2023-02-06 06:13:36 +0100

    Log in Form

--- src/components/Pages/SingIn/SingIn.jsx
+++ src/components/Pages/SingIn/SingIn.jsx
@@ -5,7 +5,21 @@ const SingIn = () => {
   return (
     <>
       <div className={styles.singin}>
-        <h1>Sing In</h1>
+        <h1>Log in to Project Banana</h1>
+        <form className={styles.form}>
+          <lable>
+            <span> Username </span>
+            <br /> <input type="text" className={styles.input} />
+          </lable>
+          <lable>
+            <span>Password </span> <br />
+            <input type="password" className={styles.input} />
+          </lable>
+          <lable>
+            <input type="checkbox" /> I have been here
+          </lable>
+          <button className={styles.button}>Log In</button>
+        </form>
       </div>
     </>
   );
