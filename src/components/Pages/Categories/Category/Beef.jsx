d2e15cc8da15bcba80b8a272d69ddae823c5685d
ARMA <oleksandrrizun@gmail.com>
2023-03-31 18:35:07 +0200

    update square and beef

--- src/components/Pages/Categories/Category/Beef.jsx
+++ src/components/Pages/Categories/Category/Beef.jsx
@@ -1,19 +1,19 @@
 import React, { useContext, useEffect, useState  } from "react";
 import { myContext } from "../../../context/context";
 import styles from "./category.module.sass";
-import { AiOutlineCloseCircle } from "react-icons/ai";
+import Square from "../../../Square/Square";
 
 
 
 const Beef = () => {
-  const { fetchBeef, categoryBeef, mealId, fetchMealId } = useContext(myContext);
+  const { fetchBeef, categoryBeef, mealId, fetchMealId,clickShowHandler, open } = useContext(myContext);
  
-  const [open, setOpen] = useState(false)
+  
   const [id, setId] = useState('52772')
   
   const clickOpenHandler = (event) => {
     setId(event.target.id)
-    return setOpen(!open);
+    return clickShowHandler();
   };
   useEffect(() => {
     fetchBeef();
@@ -22,49 +22,7 @@ const Beef = () => {
          useEffect(() => {
           fetchMealId(id);
         }, [id]);
- 
-  const beefInfo = mealId.map((info) =>
-    <div key={info.idMeal}>
-      <h1 style={{ margin: "10px" }}>
-          {info.strMeal}
-          <span style={{ float: "right", cursor: "pointer" }}>
-            <AiOutlineCloseCircle  onClick={() => setOpen(!open)}/>
-          </span>
-        </h1>
-        <h3 style={{ margin: "20px" }}>
-          <span style={{ marginRight: "50px" }}>Country: {info.strArea}</span>{" "}
-          Category: {info.strCategory}
-        </h3>
-        <img style={{ width: "250px" }} src={info.strMealThumb} alt=" meal" />
-        <p style={{ fontSize: "19px", marginBottom: "15px" }}>
-          {" "}
-          Instructions:
-        </p>{" "}
-        <p> {info.strInstructions}</p>
-        <div style={{ marginTop: "15px" }}>
-          <a
-            className={styles.linkbtn}
-            href={`${info.strYoutube}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Watch Video on YouTube
-          </a>{" "}
-          <br />
-          <a
-            className={styles.linkbtn}
-            href={`${info.strSource}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Recept more info
-          </a>
-        </div>
-
-    
-    </div>
-  )
-  
+          
   return (
     <>
       <div className={styles.category}>
@@ -80,15 +38,12 @@ const Beef = () => {
           </div>
           ))}
 
-          {open && 
-              <div className={styles.square}>
+          {open &&  <div>{mealId.map((meal)=> (
+            <Square meal={meal}/>
+          )
+          )}</div>
           
-               <h2 >Good Idia</h2>
-               <div className={styles.box}>
-                 {beefInfo}
-               </div>
-              </div>
-            }
+         }
            
           
         </div>
